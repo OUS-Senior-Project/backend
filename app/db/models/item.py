4eb655fb590f4b0add59054b2b51bd640d785ba2
Robel Kebede <126531406+RobelK1738@users.noreply.github.com>
2025-11-20 19:06:13 -0800

    Connected local db using sqlite (#24)

---
+++ app/db/models/item.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Text
+from app.db.base import Base
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(Text, nullable=True)
